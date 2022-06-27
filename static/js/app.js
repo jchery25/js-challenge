251a07bebaf3747efc5965d5ebb188d72fadffb3
Jonathan Chery <jchery25@yahoo.com>
2022-06-26 22:10:09 -0400

    Used jQueryUI to make dots draggable

---
+++ static/js/app.js
@@ -0,0 +1,11 @@
+// Initialize 
+$( init );
+
+function init() {
+    $('#black_01').draggable();
+    $('#black_02').draggable();
+    $('#blue').draggable();
+    $('#green').draggable();
+    $('#red').draggable();
+
+  }
