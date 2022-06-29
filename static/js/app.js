ceb19466547246dbd5bb186b638bc6ac12aa7f42
Jonathan Chery <jchery25@yahoo.com>
2022-06-29 01:17:20 -0400

    Worked on logic for reset button

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,22 @@
-// Initialize 
-$( init );
+$( draggable_button );
+$( reset_challange );
 
-function init() {
-    $('#black_01').draggable();
-    $('#black_02').draggable();
-    $('#blue').draggable();
-    $('#green').draggable();
-    $('#red').draggable();
-
+/**
+ * draggable_button() => Allows users to drag buttons within the limits of their windows.
+ */
+function draggable_button() {
+    $('#black_01').draggable({ containment: "window" });
+    $('#black_02').draggable({ containment: "window" });
+    $('#blue').draggable({ containment: "window" });
+    $('#green').draggable({ containment: "window" });
+    $('#red').draggable({ containment: "window" });
   }
+
+/**
+ * reset_challange() => Allows users to reset the challange.
+ */
+function reset_challange() {
+  $("#reset_button").click(function(){
+    document.location.reload();
+  });
+}
