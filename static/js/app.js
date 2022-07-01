d469df85e624b00fa6a3c47b89e094c8f0ba303e
Jonathan Chery <jchery25@yahoo.com>
2022-07-01 02:58:27 -0400

    Working on onmouseenter() functionality

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,17 @@
 $( draggable_button );
 $( reset_challange );
+$( colors_enter_section );
 
 /**
  * draggable_button() => Allows users to drag buttons within the limits of their windows.
  */
 function draggable_button() {
-    $('#black_01').draggable({ containment: "window" });
-    $('#black_02').draggable({ containment: "window" });
-    $('#blue').draggable({ containment: "window" });
-    $('#green').draggable({ containment: "window" });
-    $('#red').draggable({ containment: "window" });
-  }
+  $('#blue').draggable({ containment: "window" });
+  $('#red').draggable({ containment: "window" });
+  $('#green').draggable({ containment: "window" });
+  $('#black_01').draggable({ containment: "window" });
+  $('#black_02').draggable({ containment: "window" });
+}
 
 /**
  * reset_challange() => Allows users to reset the challange.
@@ -19,4 +20,14 @@ function reset_challange() {
   $("#reset_button").click(function(){
     document.location.reload();
   });
+}
+
+/**
+ * colors_enter_section() => Check to see if the color enters a specific grid.
+ */
+function colors_enter_section(){
+  $("#blue").mouseenter(function(){
+    $this = $(this)
+    
+  })
 }
